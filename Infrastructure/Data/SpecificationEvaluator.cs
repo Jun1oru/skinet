844b43e1d717e69fd4d4d30fafe3f855d04d9d47
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2026-04-25 17:14:57 +0300

    End of section 4

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,65 @@
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data;
+
+public class SpecificationEvaluator<T> where T : BaseEntity
+{
+    public static IQueryable<T> GetQuery(IQueryable<T> query, ISpecification<T> spec)
+    {
+        if (spec.Criteria != null)
+        {
+            query = query.Where(spec.Criteria); // Apply the criteria to the query
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = query.OrderBy(spec.OrderBy); // Apply the ordering to the query
+        }
+
+        if (spec.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(spec.OrderByDescending); // Apply the descending ordering to the query
+        }
+
+        if (spec.IsDistinct)
+        {
+            query = query.Distinct();
+        }
+
+        return query;
+    }
+
+    public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query,
+        ISpecification<T, TResult> spec)
+    {
+        if (spec.Criteria != null)
+        {
+            query = query.Where(spec.Criteria); // Apply the criteria to the query
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = query.OrderBy(spec.OrderBy); // Apply the ordering to the query
+        }
+
+        if (spec.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(spec.OrderByDescending); // Apply the descending ordering to the query
+        }
+
+        var selectQuery = query as IQueryable<TResult>;
+
+        if (spec.Select != null)
+        {
+            selectQuery = query.Select(spec.Select);
+        }
+
+        if (spec.IsDistinct)
+        {
+            selectQuery = selectQuery?.Distinct();
+        }
+
+        return selectQuery ?? query.Cast<TResult>();
+    }
+}
