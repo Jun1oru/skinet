d66ef2c5f0775d5a4963e40ed1e34131d35ae9ac
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2026-04-25 23:31:21 +0300

    End of section 8

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -7,11 +7,15 @@ import { provideRouter } from '@angular/router';
 import 'zone.js';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideZoneChangeDetection(),
+    provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
