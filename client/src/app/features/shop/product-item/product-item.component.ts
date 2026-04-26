10d9864ac6885bc01e2d25798d50b94a13b60ec8
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2026-04-26 11:32:39 +0300

    End of section 10

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -2,12 +2,13 @@ import { Component, Input } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { MatCard, MatCardContent, MatCardActions } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
-import { MatAnchor } from "@angular/material/button";
-import { MatIcon } from "@angular/material/icon";
+import { MatAnchor } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatAnchor, MatIcon],
+  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatAnchor, MatIcon, RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css',
 })
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [MatBadge, MatIcon, MatButton],
+  imports: [MatBadge, MatIcon, MatButton, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
