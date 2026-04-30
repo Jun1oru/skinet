3164d6e81eece189fd378471cfe8b4a713d8b6c0
Jun1oru <76923595+Jun1oru@users.noreply.github.com>
2026-04-30 15:42:52 +0300

    End of section 16

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51TRICEGPIy3QZV6BXvFMTtDMrJkeKXdFuOINaeDX2HLYQMZjJUQzMjKuPww12GvS7ZsfuFFuuI81nSIkyiHq3gg000hZ2kv7bj',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51TRICEGPIy3QZV6BXvFMTtDMrJkeKXdFuOINaeDX2HLYQMZjJUQzMjKuPww12GvS7ZsfuFFuuI81nSIkyiHq3gg000hZ2kv7bj',
 };
